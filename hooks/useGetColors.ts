9ebe5c4482c501110074964b798f69d6247fa4f4
nurSaadat <saadatsunnysmile@gmail.com>
2025-04-18 22:32:04 +0200

    Add color extraction hook for images

---
+++ hooks/useGetColors.ts
@@ -0,0 +1,38 @@
+import React from "react";
+import { Platform } from "react-native";
+import { getColors } from "react-native-image-colors";
+import {
+  AndroidImageColors,
+  IOSImageColors,
+} from "react-native-image-colors/build/types";
+
+export const useImageColors = ({ imageUrl }: { imageUrl: string }) => {
+  const [color, setColor] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const fetchColor = async () => {
+      try {
+        const colors = await getColors(imageUrl, {
+          fallback: "#31014B",
+          cache: true,
+          key: imageUrl,
+        });
+
+        const extractedColor =
+          Platform.OS === "ios"
+            ? (colors as IOSImageColors).primary
+            : (colors as AndroidImageColors).dominant;
+
+        setColor(extractedColor);
+      } catch (error) {
+        console.error("Error fetching colors:", error);
+      }
+    };
+
+    if (imageUrl) {
+      fetchColor();
+    }
+  }, [imageUrl]);
+
+  return { color };
+};
