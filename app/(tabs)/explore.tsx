95fb0e24941501e374f4cf6310fd515154c726b4
nurSaadat <saadatsunnysmile@gmail.com>
2025-05-04 21:24:59 +0200

    Add iOS project files and configurations for Expo integration

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,41 +1,195 @@
-import { useImageColors } from "@/hooks/useImageColors";
-import { Image, View } from "react-native";
+import { useBottomTabOverflow } from "@/components/ui/TabBarBackground";
+import { useDominantColor } from "@/hooks/useDominantColor";
+import { Image, View, Text } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const HEADER_HEIGHT = 250;
 
 export default function GradientScreen() {
   const image = require("@/assets/images/GGIY.jpg");
 
-  const { color } = useImageColors({
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+  const bottom = useBottomTabOverflow();
+  const { top } = useSafeAreaInsets();
+
+  const { color } = useDominantColor({
     imageUrl: image,
   });
 
+  const album = [
+    { author: "nurSaadat", title: "Check box" },
+    { author: "nurSaadat", title: "Background color" },
+    { author: "nurSaadat", title: "Toggle switch" },
+    { author: "nurSaadat", title: "Shadows" },
+    { author: "nurSaadat", title: "Linear gradient" },
+    { author: "nurSaadat", title: "Blur" },
+    { author: "nurSaadat", title: "Image colors" },
+    { author: "nurSaadat", title: "Border radius" },
+    { author: "nurSaadat", title: "Text font" },
+    { author: "nurSaadat", title: "Padding" },
+    { author: "nurSaadat", title: "Alignment" },
+    { author: "nurSaadat", title: "Width" },
+  ];
+
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+            [0, 0, HEADER_HEIGHT * 0.5]
+          ),
+        },
+        {
+          scale: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT / 2],
+            [1, 1, 0.5]
+          ),
+        },
+      ],
+      opacity: interpolate(
+        scrollOffset.value,
+        [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+        [1, 1, 0]
+      ),
+    };
+  });
+
   return (
-    <View
+    <LinearGradient
+      colors={
+        color
+          ? ["rgba(0,0,0,0.0)", "rgba(0,0,0,1)", "rgba(0,0,0,1)"]
+          : ["#000", "#000"]
+      }
       style={{
         height: "100%",
         width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: color || "#FFFFFF",
+        backgroundColor: color || "#000",
       }}
     >
-      <Image source={image} style={{ width: 250, height: 250 }} />
-      <LinearGradient
-        colors={[
-          "rgba(0, 0, 0, 0.0)",
-          "rgba(0, 0, 0, 0.8)",
-          "rgba(0, 0, 0, 1)",
-        ]}
-        style={{
-          height: "100%",
-          width: "100%",
-          position: "absolute",
-          zIndex: -1,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}
-      />
-    </View>
+      <Animated.ScrollView
+        ref={scrollRef}
+        scrollEventThrottle={1}
+        scrollIndicatorInsets={{ bottom, top }}
+        contentContainerStyle={{ paddingBottom: bottom }}
+        bounces={false}
+      >
+        <LinearGradient
+          colors={
+            color
+              ? [
+                  "rgba(0,0,0,0.0)",
+                  "rgba(0,0,0,0.2)",
+                  "rgba(0,0,0,0.5)",
+                  "rgba(0,0,0,1)",
+                ]
+              : ["#000", "#000"]
+          }
+          style={{
+            width: "100%",
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <Animated.View
+            style={[
+              {
+                height: HEADER_HEIGHT + top,
+                overflow: "hidden",
+                alignItems: "center",
+                paddingTop: top,
+              },
+
+              headerAnimatedStyle,
+            ]}
+          >
+            <Image
+              source={image}
+              style={[
+                {
+                  width: 250,
+                  height: 250,
+                  borderRadius: 4,
+                },
+                {
+                  shadowOffset: {
+                    width: 0,
+                    height: -4,
+                  },
+                  shadowOpacity: 0.15,
+                  shadowRadius: 3,
+                  shadowColor: "black",
+                },
+              ]}
+            />
+          </Animated.View>
+        </LinearGradient>
+        <View
+          style={{ padding: 32, overflow: "hidden", backgroundColor: "#000" }}
+        >
+          {album.map((item, index) => (
+            <View
+              key={`item-${index}`}
+              style={{
+                height: 60,
+                backgroundColor: "#000",
+                width: "100%",
+                alignItems: "center",
+                borderBottomWidth: 1,
+                borderBottomColor: "#1f1f1f",
+                flexDirection: "row",
+                gap: 16,
+              }}
+            >
+              <Image
+                source={image}
+                style={[
+                  {
+                    width: 44,
+                    height: 44,
+                    borderRadius: 4,
+                    shadowColor: "black",
+                  },
+                  {
+                    shadowOffset: {
+                      width: 0,
+                      height: -4,
+                    },
+                    shadowOpacity: 0.15,
+                    shadowRadius: 3,
+                  },
+                ]}
+              />
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  gap: 2,
+                }}
+              >
+                <Text
+                  style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}
+                >
+                  {item.title}
+                </Text>
+                <Text style={{ color: "#afafaf" }}>{item.author}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </Animated.ScrollView>
+    </LinearGradient>
   );
 }
--- hooks/useDominantColor.ts
+++ hooks/useDominantColor.ts
@@ -6,21 +6,21 @@ import {
   IOSImageColors,
 } from "react-native-image-colors/build/types";
 
-export const useImageColors = ({ imageUrl }: { imageUrl: string }) => {
+export const useDominantColor = ({ imageUrl }: { imageUrl: string }) => {
   const [color, setColor] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     const fetchColor = async () => {
       try {
         const colors = await getColors(imageUrl, {
-          fallback: "#31014B",
+          fallback: "#31014B", // any fallback color
           cache: true,
           key: imageUrl,
         });
 
         const extractedColor =
           Platform.OS === "ios"
-            ? (colors as IOSImageColors).primary
+            ? (colors as IOSImageColors).background
             : (colors as AndroidImageColors).dominant;
 
         setColor(extractedColor);
